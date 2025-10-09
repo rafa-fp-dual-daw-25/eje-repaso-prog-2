6edffbe43e2ef563d2122a9cd1223e255382a647
Taller_INF9e29 <rafamendoza.dev@gmail.com>
2025-10-09 20:27:17 +0200

    update

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dia, mes, ano;
+
+        while
+    }
+
+    public static boolean esFechaValida(int dia, int mes, int ano) {
+
+        int[] diasPorMes = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    }
+
+}
